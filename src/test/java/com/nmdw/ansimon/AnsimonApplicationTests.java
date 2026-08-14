ae6f9cd8dfb711544adae1ba3ae0ef513fe85c25
pure77 <72856694+pure77@users.noreply.github.com>
2026-08-14 13:55:26 +0900

    Initial project setup

---
+++ src/test/java/com/nmdw/ansimon/AnsimonApplicationTests.java
@@ -0,0 +1,14 @@
+package com.nmdw.ansimon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = {
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration,org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration"
+})
+class AnsimonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}

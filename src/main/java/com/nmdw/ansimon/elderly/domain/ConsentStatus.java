8d01677ee7d3fec11ab3895badb1050259a9c5f7
pure77 <72856694+pure77@users.noreply.github.com>
2026-08-18 16:10:02 +0900

    feat(elderly): expose ConsentStatus as a stable JSON/query code

--- src/main/java/com/nmdw/ansimon/elderly/domain/ConsentStatus.java
+++ src/main/java/com/nmdw/ansimon/elderly/domain/ConsentStatus.java
@@ -1,7 +1,20 @@
 package com.nmdw.ansimon.elderly.domain;
 
-public enum ConsentStatus {
-    CONSENTED,
-    NOT_CONSENTED,
-    WITHDRAWN
+import com.nmdw.ansimon.global.converter.EnumCode;
+
+public enum ConsentStatus implements EnumCode {
+    CONSENTED("consented"),
+    NOT_CONSENTED("not_consented"),
+    WITHDRAWN("withdrawn");
+
+    private final String code;
+
+    ConsentStatus(String code) {
+        this.code = code;
+    }
+
+    @Override
+    public String code() {
+        return code;
+    }
 }
